1dd20cb3e06f565fc03e8abb71688331da77caca
oliverphil <oliverphil@myvuw.ac.nz>
2017-05-04 15:33:43 +1200

    Update AVC.cpp

--- AVC.cpp
+++ AVC.cpp
@@ -28,20 +28,30 @@ void turn(double degree, bool bool_right){
 	int right;
 	if (bool_right){
 		left = 100;
-		right = -100;		
+		right = -20;		
 	} else if (!bool_right){
-		left = -100;
+		left = -20;
 		right = 100;
 	} else {
 		return;
 	}
 		
-	int turn_time = degree/360 * //time for full circle
-	set_motor(1, 100); //left
-	set_motor(2, -150); //right
+	int turn_time = degree/360 * //<time for full circle>
+	set_motor(1, left); //left
+	set_motor(2, right); //right
 	sleep1(0, turn_time); //alter to turn through a certain angle
-	set_motor(1, 0);
-	set_motor(2, 0);
+	//set_motor(1, 0); Probably don't need these if there is a drive_straight directly after the turn method
+	//set_motor(2, 0);
 	return;
 }
 
+int adc(int pin){
+	int adc_total;
+	for (int i=0;i<5;i++){
+		int adc_reading = read_analog(pin);
+		adc_total += adc_reading;
+		sleep1(0, 10);
+	}
+	int adc_mean = adc_total/5;
+	return adc_mean;
+}
