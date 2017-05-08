f9e1fe91b870534711ebe887df7a37083f0c6957
Tawera Manaena <taweramanaena@gmail.com>
2017-05-08 13:17:39 +1200

    Update Sector 2 - Testing, Camera.cpp

--- Sector 2 - Testing, Camera.cpp
+++ Sector 2 - Testing, Camera.cpp
@@ -4,14 +4,15 @@
 // The outputs will be in the form of (.txt) files.
 // This process is to ensure our camera is interpreting the data correctly and is calibrated correctly.
 
-void 
+void
 
-Camera to digital procedure:
-Take image with camera, stores the image in memory:
-take_picture():
-Camera dimensions:
-column_y = 320;
-row_x = 240;
+//initialise RPi
+init();
+take_picture(): //Take image with camera, stores the image in memory:
 
-For (int i = 0; I < column_y; i__){
-get_pixel.(
+//Camera dimensions
+int column_y = 320;
+int row_x = 240;
+
+for (int i = 0; I < column_y; i__){
+  get_pixel.(
