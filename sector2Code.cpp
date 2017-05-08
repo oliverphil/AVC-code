e04103d764fc6b0c221abe8c98103cc467885d7f
oliverphil <oliverphil@myvuw.ac.nz>
2017-05-08 12:58:01 +1200

    Update sector2Code.cpp

--- sector2Code.cpp
+++ sector2Code.cpp
@@ -1,5 +1,52 @@
-init();
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main()
+{
+	init();	
+}
 
 void sector2(){
-  take.picture();
+  int previous_error = 0;
+  while(true){
+    take_picture()
+    char pix[320];
+    for(int i=0; i<320; i++){
+      pix[i] = get_pixel(i, 120, 3);
+    }
+    int proportional_signal = proportional_signal();
+    previous_error, int derivative_signal = derivative_signal(previous_error);
+    int total_error = proportional_signal + derivative_signal;
+    set_motor(1, 80 + total_error);
+    set_motor(2, -80 + total_error);
+  }
 }
+
+int proportional_signal(){
+  int current_error = 0;
+    double kp = 0.5;
+    int proportional_signal;
+    for (i=0; i<320, i++){
+      int error = (i-160)*get_pixel(i, 120, 3);
+      current_error += error;
+    }
+    proportional_signal = error*kp;
+  return proportional_signal;
+}
+
+int, int derivative_signal(int prev){
+  int previous_error = prev;
+  int current_error = 0;
+  int kd = 5;
+  int derivative_signal;
+  for (i=0; i<320; i++){
+    int error = (i-160)*get_pixel(i, 120, 3);
+    current_error += error;
+  }
+  sleep1(0, 100000);
+  derivative_signal = (current_error-previous_error*10)*kd;
+  previous_error = current_error;
+  return previous_error, derivative_signal;
+}
+  
