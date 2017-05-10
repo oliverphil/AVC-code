9129529d25c91698b8960b05a25fd1788c8d6cc1
oliverphil <oliverphil@myvuw.ac.nz>
2017-05-11 10:13:14 +1200

    Update and rename sector2Code.cpp to sector2Code1-1.cpp

--- sector2Code1-1.cpp
+++ sector2Code1-1.cpp
@@ -10,25 +10,35 @@ int main()
 void sector2(){
   int previous_error = 0;
   while(true){
-    take_picture()
-    int proportional_signal = proportional_signal();
-    previous_error, int derivative_signal = derivative_signal(previous_error);
-    int total_error = proportional_signal + derivative_signal;
-    set_motor(1, 80 + total_error);
-    set_motor(2, -80 + total_error);
+    int nwp = 0;
+       if(nwp<127){
+    	take_picture();
+    	int proportionalSignal, nwp = proportional_signal();
+    	previous_error, int derivativeSignal = derivative_signal(previous_error);
+    	int total_error = proportionalSignal + derivativeSignal;
+    	set_motor(1, 80 + total_error);
+    	set_motor(2, -80 + total_error);
+       } else {
+	  break;
+       }
   }
 }
 
-int proportional_signal(){
+int, int proportional_signal(){
   int current_error = 0;
     double kp = 0.5;
     int proportional_signal;
+    int numberWhite = 0;
     for (i=0; i<320, i++){
-      int error = (i-160)*get_pixel(i, 120, 3);
+      int pixel = get_pixel(i, 120, 3);
+      int error = (i-160)*pixel;
+      if (pixel>127){
+	  numberWhite++;
+      }
       current_error += error;
     }
-    proportional_signal = error*kp;
-  return proportional_signal;
+    proportional_signal = current_error*kp;
+    return proportional_signal, numberWhite;
 }
 
 int, int derivative_signal(int prev){
