f2821d33ae128712f2d4218002d38eff9e0a6ad7
oliverphil <oliverphil@myvuw.ac.nz>
2017-05-19 18:07:36 +1200

    Update Sector2Code1-4.cpp

--- Sector2Code1-4.cpp
+++ Sector2Code1-4.cpp
@@ -1,1 +1,58 @@
+//AVC Challenge
+//Team:	BTN
 
+//Quadrant Name:	Curved Line
+//Quadrant Number:	2 of 4
+
+//RPi IP Address: 10.140.30.158
+//IMPORT libraries
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+void sector2(){
+  double previous_error = 1;
+  int derivative_signal;
+  while(true){
+    double current_error = 0;
+    int kp = 0.5;
+    int kd = 0.2;
+    int proportional_signal;    
+    int nwp = 0;
+    for (i=0, i<320, i++){
+        int pixel = get_pixel(i, 120, 3);
+        if(pixel>127){
+          nwp++;
+          pixel = 1;
+        } else {
+          pixel = 0;
+        }
+        int error = (i-160)*pixel;        
+        current_error += error;
+    }
+    proportional_signal = (current_error/nwp)*kp;
+    derivative_signal = (current_error-previous_error)*kd;
+    previous_error = current_error;
+    int speed = proportional_signal+derivative_signal;
+    if(nwp>200){
+        speed = -80;
+        set_motor(1, speed);
+        set_motor(2, -speed);
+    } else {
+    set_motor(1, 100+speed);
+    set_motor(2, -100+speed);
+    }
+  }
+}
+
+//OUTPUT. SET wheel speed to zero
+void kill(){
+	set_motor(2, 0);
+	set_motor(1, 0);
+}
+
+//RUN main method
+int main(){
+	init(); //INIT rasberry pi components //NOTE: should we implement a test method to ensure each component is operating correctly?
+	sector2();
+}
